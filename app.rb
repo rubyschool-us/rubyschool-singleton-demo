e066730df6c1911b57ed61d3d1ff588d0f6e572f
Roman Pushkin <roman.pushkin@gmail.com>
2014-11-27 23:13:43 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  # class method
   def self.say_something
     puts "haha"
+  end
+
+  # instance method
+  def log_something wat
+    @f.puts wat
   end
 end
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something 'hey!'
